89ea1fa2ccc60ce072c912ffc889281a92c38ffc
Volodymyr <babihuk888@gmail.com>
2024-08-11 18:36:42 +0300

    task2

--- lesson1-react-intro/task2/index.js
+++ lesson1-react-intro/task2/index.js
@@ -22,8 +22,8 @@ const rootElement = document.querySelector('#root');
 const element = React.createElement(
 	'div',
 	{ className: 'greeting' },
-	React.createElement('div', { className: 'title' }, 'Hello, World!'),
-	React.createElement('div', { className: 'text' }, "I'm learning React!")
+	React.createElement('div', { className: 'greeting__title' }, 'Hello, World!'),
+	React.createElement('div', { className: 'greeting__text' }, "I'm learning React!")
 )
 
 ReactDOM.render(element, rootElement);
