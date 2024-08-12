202fabc43fd60182e3bef48a60530d9b7ad00bb4
Volodymyr <babihuk888@gmail.com>
2024-08-12 17:54:40 +0300

    lesson2 task1

---
+++ lesson2-jsx/theory/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootElement = document.getElementById('root')
+
+const element = (
+	<div>
+		<h1>Search Form</h1>
+		<div>
+			<input type='text' />
+			<button>Submit</button>
+		</div>
+	</div>
+)
+
+ReactDOM.render(element, rootElement);
