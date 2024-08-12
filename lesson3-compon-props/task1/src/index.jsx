44a37ba9e8e2f281af12648ed26adb0198152f43
Volodymyr <babihuk888@gmail.com>
2024-08-12 20:59:42 +0300

    lesson3 task1

--- lesson3-compon-props/task1/src/index.jsx
+++ lesson3-compon-props/task1/src/index.jsx
@@ -1,10 +1,7 @@
 import Search from './Search'
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <Search name="Tom" />
-  </StrictMode>
 )
