ed1f408c7385eebe1ada6e85711acb6a82de50da
Volodymyr <babihuk888@gmail.com>
2024-08-13 16:56:41 +0300

    task3

--- lesson3-compon-props/task3/src/Comment.jsx
+++ lesson3-compon-props/task3/src/Comment.jsx
@@ -5,15 +5,12 @@ import UserInfo from './UserInfo';
 
 const formatDate = date => moment(date).format('DD MMM YYYY');
 
-function Comment({ text, date, author: { name, avatarUrl } }) {
+function Comment(props) {
   return (
     <div className="comment">
-      <div className="user-info">
-        <UserInfo name={name} avatarUrl={avatarUrl} />
-        <div className="user-info__name">{name}</div>
-      </div>
-      <div className="comment__text">{text}</div>
-      <div className="comment__date">{formatDate(date)}</div>
+      <UserInfo name={props.user.name} avatarUrl={props.user.avatarUrl} />
+      <div className="comment__text">{props.text}</div>
+      <div className="comment__date">{formatDate(props.date)}</div>
     </div>
   );
 }
--- lesson3-compon-props/task3/src/UserInfo.jsx
+++ lesson3-compon-props/task3/src/UserInfo.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import Avatar from './Avatar';
 
-const UserInfo = ({ name, avatarUrl }) => <Avatar name={name} avatarUrl={avatarUrl} />;
+const UserInfo = props => {
+  return (
+    <div className="user-info">
+      <Avatar name={props.name} avatarUrl={props.avatarUrl} />
+      <div className="user-info__name">{props.name}</div>
+    </div>
+  );
+};
 
 export default UserInfo;
