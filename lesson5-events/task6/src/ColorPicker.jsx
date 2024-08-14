093c106a82643dc3ac7f93790d12e71d48726661
Volodymyr <babihuk888@gmail.com>
2024-08-14 21:18:07 +0300

    lesson5 task6

--- lesson5-events/task6/src/ColorPicker.jsx
+++ lesson5-events/task6/src/ColorPicker.jsx
@@ -1,42 +1,46 @@
 import React from 'react';
+import { useRef } from 'react';
 
-class ColorPicker extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: '',
-    };
-  }
+const ColorPicker = () => {
+  const titleRef = useRef(null);
+  const buttonCoral = useRef(null);
+  const buttonAqua = useRef(null);
+  const buttonBisque = useRef(null);
 
-  handleMouseEnter = text => {
-    this.setState({ title: text });
-  }
+  const handleMouseEnter = color => {
+    titleRef.current.textContent = color;
+  };
 
-  handleMouseLeave = () => {
-    this.setState({ title: '' });
-  }
-  
-  render() {
-    return (
-      <div className="picker">
-        <div className="picker__title">{this.state.title}</div>
-        <div className="picker__buttons">
-          <button className="picker__button picker__button_coral"
-            onMouseEnter={() => this.handleMouseEnter('Coral')}
-            onMouseLeave={this.handleMouseLeave}
-            ></button>
-          <button className="picker__button picker__button_aqua"
-            onMouseEnter={() => this.handleMouseEnter('Aqua')}
-            onMouseLeave={this.handleMouseLeave}
-            ></button>
-          <button className="picker__button picker__button_bisque"
-            onMouseEnter={() => this.handleMouseEnter('Bisque')}
-            onMouseLeave={this.handleMouseLeave}
-            ></button>
-        </div>
+  const handleMouseLeave = () => {
+    titleRef.current.textContent = '';
+  };
+
+  return (
+    <div>
+      <div ref={titleRef} className="picker__title">
       </div>
-    );
-  }
-}
+      <div>
+        <button
+          ref={buttonCoral}
+          onMouseEnter={() => handleMouseEnter('Coral')}
+          onMouseLeave={handleMouseLeave}
+          className="picker__button picker__button_coral"
+        ></button>
+        <button
+          ref={buttonAqua}
+          onMouseEnter={() => handleMouseEnter('Aqua')}
+          onMouseLeave={handleMouseLeave}
+          className="picker__button picker__button_aqua"
+        ></button>
+        <button
+          ref={buttonBisque}
+          onMouseEnter={() => handleMouseEnter('Bisque')}
+          onMouseLeave={handleMouseLeave}
+          className="picker__button picker__button_bisque"
+        ></button>
+      </div>
+    </div>
+  );
+};
 
-export default ColorPicker;;
+export default ColorPicker;
