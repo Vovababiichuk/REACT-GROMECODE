949f050d386c942775ba344e3f5639032b18b4a7
Volodymyr <babihuk888@gmail.com>
2024-08-14 17:08:49 +0300

    lesson5 task4

--- lesson5-events/task4/src/Colors.jsx
+++ lesson5-events/task4/src/Colors.jsx
@@ -1,71 +1,72 @@
-// import React from 'react';
-
-// const RED = 'red';
-// const GREEN = 'green';
-// const BLUE = 'blue';
-
-// const Colors = () => {
-//   const handleSetBodyColor = color => {
-//     document.body.style.backgroundColor = color;
-//   };
-
-//   return (
-//     <div className="counter">
-//       <button
-//         onClick={() => handleSetBodyColor(RED)}
-//         className="counter__button counter__value"
-//         style={{ backgroundColor: RED }}
-//       ></button>
-//       <button
-//         onClick={() => handleSetBodyColor(GREEN)}
-//         className="counter__button counter__value"
-//         style={{ backgroundColor: GREEN }}
-//       ></button>
-//       <button
-//         onClick={() => handleSetBodyColor(BLUE)}
-//         className="counter__button counter__value"
-//         style={{ backgroundColor: BLUE }}
-//       ></button>
-//     </div>
-//   );
-// };
-
-// export default Colors;
-
-
-import React, { Component } from 'react';
+import React from 'react';
 
 const RED = 'red';
 const GREEN = 'green';
 const BLUE = 'blue';
 
-class Colors extends Component {
-  handleSetBodyColor = color => {
+const Colors = () => {
+  const handleSetBodyColor = color => {
     document.body.style.backgroundColor = color;
   };
 
-  render() {
-    return (
-      <div className="counter">
-        <button
-          onClick={() => this.handleSetBodyColor(RED)}
-          className="counter__button counter__value"
-          style={{ backgroundColor: RED }}
-        ></button>
-        <button
-          onClick={() => this.handleSetBodyColor(GREEN)}
-          className="counter__button counter__value"
-          style={{ backgroundColor: GREEN }}
-        ></button>
-        <button
-          onClick={() => this.handleSetBodyColor(BLUE)}
-          className="counter__button counter__value"
-          style={{ backgroundColor: BLUE }}
-        ></button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="counter">
+      <button
+        onClick={() => handleSetBodyColor(RED)}
+        className="colors__button"
+        style={{ backgroundColor: RED }}
+      ></button>
+      <button
+        onClick={() => handleSetBodyColor(GREEN)}
+        className="colors__button"
+        style={{ backgroundColor: GREEN }}
+      ></button>
+      <button
+        onClick={() => handleSetBodyColor(BLUE)}
+        className="colors__button"
+        style={{ backgroundColor: BLUE }}
+      ></button>
+    </div>
+  );
+};
 
 export default Colors;
+
+//! =================================
+
+// import React, { Component } from 'react';
+
+// const RED = 'red';
+// const GREEN = 'green';
+// const BLUE = 'blue';
+
+// class Colors extends Component {
+//   handleSetBodyColor = color => {
+//     document.body.style.backgroundColor = color;
+//   };
+
+//   render() {
+//     return (
+//       <div className="counter">
+//         <button
+//           onClick={() => this.handleSetBodyColor(RED)}
+//           className="colors__button"
+//           style={{ backgroundColor: RED }}
+//         ></button>
+//         <button
+//           onClick={() => this.handleSetBodyColor(GREEN)}
+//           className="colors__button"
+//           style={{ backgroundColor: GREEN }}
+//         ></button>
+//         <button
+//           onClick={() => this.handleSetBodyColor(BLUE)}
+//           className="colors__button"
+//           style={{ backgroundColor: BLUE }}
+//         ></button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Colors;
 
