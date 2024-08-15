c4f299f3370a09279125ff9608d787ea0ce34903
Volodymyr <babihuk888@gmail.com>
2024-08-15 17:17:49 +0300

    lesson6 task3

--- lesson6-condition-render/task3/src/Page.jsx
+++ lesson6-condition-render/task3/src/Page.jsx
@@ -2,28 +2,21 @@ import React from 'react';
 import { useState } from 'react';
 import Info from './Info';
 
+const texts = {
+  iPhone13: 'Price is 500$. Available in 2 colors',
+  iPhone13Pro: 'Price is 650$. Not available',
+};
+
 const Page = () => {
 	const [info, setInfo] = useState('');
-
-	const handleInfoIphone13 = () => {
-		setInfo('Price is 500$. Available in 2 colors');
-	}
-
-	const handleInfoIphone13Pro = () => {
-		setInfo('Price is 650$. Not available');
-	}
-
-	const handleInfoClear = () => {
-		setInfo('');
-	}
 
   return (
     <div className="page">
       <Info info={info} />
       <div className="actions">
-        <button onClick={handleInfoIphone13} className="btn">IPhone 13</button>
-        <button onClick={handleInfoIphone13Pro} className="btn">IPhone 13 Pro</button>
-        <button onClick={handleInfoClear} className="btn">Clear</button>
+        <button onClick={() => setInfo(texts.iPhone13)} className="btn">IPhone 13</button>
+        <button onClick={() => setInfo(texts.iPhone13Pro)} className="btn">IPhone 13 Pro</button>
+        <button onClick={() => setInfo('')} className="btn">Clear</button>
       </div>
     </div>
   );
