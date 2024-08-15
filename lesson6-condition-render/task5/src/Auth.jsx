e3d299b496cc807c6e968e10a224f464d0ac2398
Volodymyr <babihuk888@gmail.com>
2024-08-15 20:18:50 +0300

    lesson6 task5

--- lesson6-condition-render/task5/src/Auth.jsx
+++ lesson6-condition-render/task5/src/Auth.jsx
@@ -1,3 +1,5 @@
+//! v1
+
 import React, { useState } from 'react';
 import Login from './Login';
 import Spinner from './Spinner';
@@ -35,3 +37,58 @@ const Auth = () => {
 };
 
 export default Auth;
+
+//! v2
+
+// import React, { useState, useCallback, useReducer } from 'react';
+// import Login from './Login';
+// import Spinner from './Spinner';
+// import Logout from './Logout';
+
+// const initialState = {
+//   isLoading: false,
+//   isLoggedIn: false,
+// };
+
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case 'LOGIN_START':
+//       return { ...state, isLoading: true };
+//     case 'LOGIN_SUCCESS':
+//       return { ...state, isLoading: false, isLoggedIn: true };
+//     case 'LOGOUT':
+//       return { ...state, isLoggedIn: false, isLoading: false };
+//     default:
+//       return state;
+//   }
+// };
+
+// const Auth = () => {
+//   const [state, dispatch] = useReducer(reducer, initialState);
+
+//   const handleLoginSpinner = useCallback(() => {
+//     dispatch({ type: 'LOGIN_START' });
+
+//     setTimeout(() => {
+//       dispatch({ type: 'LOGIN_SUCCESS' });
+//     }, 2000);
+//   }, []);
+
+//   const handleLogout = useCallback(() => {
+//     dispatch({ type: 'LOGOUT' });
+//   }, []);
+
+//   return (
+//     <div>
+//       {state.isLoading ? (
+//         <Spinner size={50} />
+//       ) : state.isLoggedIn ? (
+//         <Logout onLogout={handleLogout} />
+//       ) : (
+//         <Login onLogin={handleLoginSpinner} />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Auth;
