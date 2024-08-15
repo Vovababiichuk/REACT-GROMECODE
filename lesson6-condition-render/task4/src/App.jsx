73fbeba076c902e62a2e582f3fe30e37042aab34
Volodymyr <babihuk888@gmail.com>
2024-08-15 19:22:49 +0300

    lesson6 task4

---
+++ lesson6-condition-render/task4/src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Status from './Status.jsx';
+
+const App = () => <Status isOnline={false} />;
+
+export default App;
---
+++ lesson6-condition-render/task4/src/Status.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Online from './Online.jsx';
+import Offline from './Offline.jsx';
+
+const Status = ({ isOnline }) => {
+	return (
+		isOnline
+			? <Online />
+			: <Offline />
+	)
+}
+
+export default Status;
