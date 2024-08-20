70f453b1484a916e1b1714a45eea89bd80e3700d
Volodymyr <babihuk888@gmail.com>
2024-08-20 16:01:10 +0300

    lesson7 task2

--- lesson7-lists/task2/src/UsersList.jsx
+++ lesson7-lists/task2/src/UsersList.jsx
@@ -2,20 +2,20 @@ import React, { useState } from 'react';
 import User from './User.jsx';
 
 const UsersList = ({ users }) => {
-  const [isAsc, setIsAsc] = useState(true);
+  const [btnText, setBtnText] = useState('-');
   const [sortedUsers, setSortedUsers] = useState(users);
 
   const handleToggleSort = () => {
-    setIsAsc(!isAsc);
+    setBtnText(btnText === 'asc' ? 'desc' : 'asc');
     setSortedUsers(prevUsers =>
-      [...prevUsers].sort((a, b) => (isAsc ? a.age - b.age : b.age - a.age)),
+      [...prevUsers].sort((a, b) => (btnText === 'asc' ? b.age - a.age : a.age - b.age)),
     );
   };
 
   return (
     <div>
       <button onClick={handleToggleSort} className="btn">
-        {isAsc ? 'asc' : 'desc'}
+        {btnText}
       </button>
       <ul className="users">
         {sortedUsers.map(user => (
