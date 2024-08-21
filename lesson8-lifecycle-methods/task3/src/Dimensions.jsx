b72c2f75828d17d2171f84985bf5966d8c66bdfc
Volodymyr <babihuk888@gmail.com>
2024-08-21 20:37:23 +0300

    lesson8 task3

--- lesson8-lifecycle-methods/task3/src/Dimensions.jsx
+++ lesson8-lifecycle-methods/task3/src/Dimensions.jsx
@@ -1,31 +1,79 @@
-import React, { useState, useEffect } from 'react';
+//! ==================Function Component==================
 
-const Dimensions = () => {
-  const [width, setWidth] = useState(window.innerWidth);
-  const [height, setHeight] = useState(window.innerHeight);
+// import React, { useState, useEffect } from 'react';
 
-  useEffect(() => {
-    const handleResize = () => {
-      setWidth(window.innerWidth);
-      setHeight(window.innerHeight);
+// const Dimensions = () => {
+//   const [width, setWidth] = useState(window.innerWidth);
+//   const [height, setHeight] = useState(window.innerHeight);
+
+//   useEffect(() => {
+//     const handleResize = () => {
+//       setWidth(window.innerWidth);
+//       setHeight(window.innerHeight);
+//     };
+
+//     window.addEventListener('resize', handleResize);
+
+//     return () => {
+//       window.removeEventListener('resize', handleResize);
+//     };
+//   }, []);
+
+//   useEffect(() => {
+//     document.title = `${width} - ${height}`;
+//   }, [width, height]);
+
+//   return (
+//     <div>
+//       {width} x {height}
+//     </div>
+//   );
+// };
+
+// export default Dimensions;
+
+//! ==================Class Component==================
+import React, { Component } from 'react';
+
+class Dimensions extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: window.innerWidth,
+      height: window.innerHeight,
     };
+  }
 
-    window.addEventListener('resize', handleResize);
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+    document.title = `${this.state.width} - ${this.state.height}`;
+  }
 
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.width !== this.state.width || prevState.height !== this.state.height) {
+      document.title = `${this.state.width} - ${this.state.height}`;
+    }
+  }
 
-  useEffect(() => {
-    document.title = `${width} - ${height}`;
-  }, [width, height]);
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
 
-  return (
-    <div>
-      {width} x {height}
-    </div>
-  );
-};
+  handleResize = () => {
+    this.setState({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  render() {
+    const { width, height } = this.state;
+    return (
+      <div>
+        {width} x {height}
+      </div>
+    );
+  }
+}
 
 export default Dimensions;
