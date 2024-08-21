74ee69a3c233a1aa7cae5a9a66d17fe52205d360
Volodymyr <babihuk888@gmail.com>
2024-08-21 17:26:52 +0300

    lesson8 task2

--- lesson8-lifecycle-methods/task2/src/App.jsx
+++ lesson8-lifecycle-methods/task2/src/App.jsx
@@ -1,4 +1,43 @@
-import React, { useState } from 'react';
+//! ==========================Function Component===========================
+
+// import React, { useState } from 'react';
+// import Clock from './Clock.jsx';
+
+// const locations = [
+//   { location: 'New York', offset: -5 },
+//   { location: 'Kyiv', offset: 3 },
+//   { location: 'London', offset: 0 },
+// ];
+
+// const App = () => {
+//   const [showClocksToggle, setShowClocksToggle] = useState(true);
+//   const handleShowToggleClocks = () => {
+//     setShowClocksToggle(!showClocksToggle);
+//   };
+
+//   return (
+//     <>
+//       {showClocksToggle && (
+//         <div className="clocks">
+//           {locations.map(({ location, offset }) => (
+//             <Clock key={location} location={location} offset={offset} />
+//           ))}
+//         </div>
+//       )}
+//       <div className="clock__time">
+//         <button className="clock__btn" onClick={handleShowToggleClocks}>
+//           {showClocksToggle ? 'Hide clocks' : 'Show clocks'}
+//         </button>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default App;
+
+//! ==========================Class Component===========================
+
+import React, { Component } from 'react';
 import Clock from './Clock.jsx';
 
 const locations = [
@@ -7,29 +46,40 @@ const locations = [
   { location: 'London', offset: 0 },
 ];
 
-const App = () => {
-  console.log('App rendered');
-  const [showClocksToggle, setShowClocksToggle] = useState(true);
-  const handleShowToggleClocks = () => {
-    setShowClocksToggle(!showClocksToggle);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showClocksToggle: true,
+    };
+  }
+
+  handleShowToggleClocks = () => {
+    this.setState(prevState => ({
+      showClocksToggle: !prevState.showClocksToggle,
+    }));
   };
 
-  return (
-    <>
-      {showClocksToggle && (
-        <div className="clocks">
-          {locations.map(({ location, offset }) => (
-            <Clock key={location} location={location} offset={offset} />
-          ))}
+  render() {
+    const { showClocksToggle } = this.state;
+
+    return (
+      <>
+        {showClocksToggle && (
+          <div className="clocks">
+            {locations.map(({ location, offset }) => (
+              <Clock key={location} location={location} offset={offset} />
+            ))}
+          </div>
+        )}
+        <div className="clock__time">
+          <button className="clock__btn" onClick={this.handleShowToggleClocks}>
+            {showClocksToggle ? 'Hide clocks' : 'Show clocks'}
+          </button>
         </div>
-      )}
-      <div className="clock__time">
-        <button className="clock__btn" onClick={handleShowToggleClocks}>
-          {showClocksToggle ? 'Hide clocks' : 'Show clocks'}
-        </button>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  }
+}
 
 export default App;
--- lesson8-lifecycle-methods/task2/src/Clock.jsx
+++ lesson8-lifecycle-methods/task2/src/Clock.jsx
@@ -1,4 +1,38 @@
-import React, { useState, useEffect } from 'react';
+//! ==========================Function Component===========================
+
+// import React, { useState, useEffect } from 'react';
+// import moment from 'moment';
+
+// const getTimeWithOffset = offset => {
+//   return moment()
+//     .utcOffset(offset * 60)
+//     .format('h:mm:ss A');
+// };
+
+// const Clock = ({ location, offset }) => {
+//   const [time, setTime] = useState(getTimeWithOffset(offset));
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setTime(getTimeWithOffset(offset));
+//     }, 1000);
+
+//     return () => clearInterval(interval);
+//   }, [offset]);
+
+//   return (
+//     <div className="clock">
+//       <div className="clock__location">{location}</div>
+//       <div className="clock__time">{time}</div>
+//     </div>
+//   );
+// };
+
+// export default Clock;
+
+//! ==========================Class Component===========================
+
+import React, { Component } from 'react';
 import moment from 'moment';
 
 const getTimeWithOffset = offset => {
@@ -7,25 +41,35 @@ const getTimeWithOffset = offset => {
     .format('h:mm:ss A');
 };
 
-const Clock = ({ location, offset }) => {
-  console.log('Clock rendered');
-  const [time, setTime] = useState(getTimeWithOffset(offset));
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: getTimeWithOffset(this.props.offset),
+    };
+  }
 
-  useEffect(() => {
-    console.log('EFFECT CALLED');
-    const interval = setInterval(() => {
-      setTime(getTimeWithOffset(offset));
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({ time: getTimeWithOffset(this.props.offset) });
     }, 1000);
+  }
 
-    return () => clearInterval(interval);
-  }, [offset]);
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
-  return (
-    <div className="clock">
-      <div className="clock__location">{location}</div>
-      <div className="clock__time">{time}</div>
-    </div>
-  );
-};
+  render() {
+    const { location } = this.props;
+    const { time } = this.state;
+
+    return (
+      <div className="clock">
+        <div className="clock__location">{location}</div>
+        <div className="clock__time">{time}</div>
+      </div>
+    );
+  }
+}
 
 export default Clock;
