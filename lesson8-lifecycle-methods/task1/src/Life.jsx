2d705b6b3bfbd9e9379b676c1cd066e1f43ec2b7
Volodymyr <babihuk888@gmail.com>
2024-08-21 13:32:58 +0300

    lesson8 task1

--- lesson8-lifecycle-methods/task1/src/Life.jsx
+++ lesson8-lifecycle-methods/task1/src/Life.jsx
@@ -1,40 +1,37 @@
 import React, { Component } from 'react';
 
 export default class Life extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			startText: 'constructor: good place to create state'
-		};
-	}
+    console.log('constructor: good place to create state');
+  }
 
-	componentDidMount() {
-		console.log('componentDidMount: API calls, subscriptions');
-	}
+  componentDidMount() {
+    console.log('componentDidMount: API calls, subscriptions');
+  }
 
-	// Выводит в консоль текст перед каждым рендером, кроме первого.
-	shouldComponentUpdate(nextProps, nextState) {
-		console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
-		return true; // Обязательно возвращаем true, чтобы рендер не был предотвращен
-	}
+  // Выводит в консоль текст перед каждым рендером, кроме первого.
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+    return true; // Обязательно возвращаем true, чтобы рендер не был предотвращен
+  }
 
-	// Выводит в консоль текст после каждого рендера, кроме первого.
-	componentDidUpdate(prevProps, prevState) {
-		console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
-	}
+  // Выводит в консоль текст после каждого рендера, кроме первого.
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
+  }
 
-	// Выводит в консоль текст перед удалением компонента.
-	componentWillUnmount() {
-		console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
-	}
+  // Выводит в консоль текст перед удалением компонента.
+  componentWillUnmount() {
+    console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
+  }
 
-	render() {
-		return (
-			<div>
-				<div>return React element to build DOM</div>
-				{this.state.startText}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <div>return React element to build DOM</div>
+      </div>
+    );
+  }
 }
