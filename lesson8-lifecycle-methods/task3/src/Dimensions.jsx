095ee1b73e73b90dffcf23e70da37a99ced0f14f
Volodymyr <babihuk888@gmail.com>
2024-08-21 20:27:07 +0300

    lesson8 task3

--- lesson8-lifecycle-methods/task3/src/Dimensions.jsx
+++ lesson8-lifecycle-methods/task3/src/Dimensions.jsx
@@ -1,27 +1,31 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Dimensions = () => {
-	const [width, setWidth] = useState(window.innerWidth);
-	const [height, setHeight] = useState(window.innerHeight);
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
 
-	useEffect(() => {
-		const handleResize = () => {
-			setWidth(window.innerWidth);
-			setHeight(window.innerHeight);
-		}
-		window.addEventListener('resize', handleResize);
-		return () => {
-			window.removeEventListener('resize', handleResize);
-		}
-	}, [])
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
 
-	document.title = `${width} - ${height}`
-	return (
-		<div>
-			{width} x {height}
-		</div>
-	)
-}
+    window.addEventListener('resize', handleResize);
 
-export default Dimensions
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    document.title = `${width} - ${height}`;
+  }, [width, height]);
+
+  return (
+    <div>
+      {width} x {height}
+    </div>
+  );
+};
+
+export default Dimensions;
