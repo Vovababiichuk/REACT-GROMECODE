cfaf1340a0b703040f7f9fad404b3dc272072a37
Volodymyr <babihuk888@gmail.com>
2024-08-22 22:49:23 +0300

    lesson9 task3

--- lesson9-forms/task3/src/UserForm.jsx
+++ lesson9-forms/task3/src/UserForm.jsx
@@ -145,78 +145,56 @@
 import React, { Component } from 'react';
 
 class UserForm extends Component {
-  constructor(props) {
-    super(props);
-    this.formRef = React.createRef();
-  }
+  handleSubmit = event => {
+    event.preventDefault();
+    const formData = [...new FormData(this.formRef)].reduce(
+      (acc, [name, value]) => ({ ...acc, [name]: value }),
+      {},
+    );
 
-  handleSubmit = (e) => {
-    e.preventDefault();
+    this.props.onSubmit(formData);
+  };
 
-    if (this.formRef.current) {
-      const formData = new FormData(this.formRef.current);
-      const userData = {
-        name: formData.get('name'),
-        student: formData.get('student') === 'on',
-        occupation: formData.get('occupation'),
-        about: formData.get('about'),
-      };
-      this.props.onSubmit(userData);
-    }
+  setRef = node => {
+    this.formRef = node;
   };
 
   render() {
     return (
-      <form ref={this.formRef} className="login-form" onSubmit={this.handleSubmit}>
+      <form ref={this.setRef} className="login-form" onSubmit={this.handleSubmit}>
         <h1 className="form-title">Profile</h1>
 
         <div className="form-control">
           <label className="form-label" htmlFor="name">
             Name
           </label>
-          <input
-            className="form-input"
-            type="text"
-            id="name"
-            name="name"
-          />
+          <input className="form-input" type="text" id="name" name="name" />
         </div>
 
         <div className="form-control">
           <label className="form-label" htmlFor="student">
             Student
           </label>
-          <input
-            className="form-input"
-            type="checkbox"
-            id="student"
-            name="student"
-          />
+          <input className="form-input" type="checkbox" id="student" name="student" />
         </div>
 
         <div className="form-control">
-          <label className="form-label" htmlFor="occupation">
+          <label className="form-label" id="occupation" htmlFor="occupation">
             Occupation
           </label>
-          <select
-            name="occupation"
-            className="form-input"
-          >
+          <select name="occupation" className="form-input">
             <option value="london">London</option>
             <option value="new-york">New York</option>
-            <option value="sidney">Sidney</option>
-            <option value="berlin">Berlin</option>
+            <option value="coconut">Sidney</option>
+            <option value="mango">Berlin</option>
           </select>
         </div>
 
         <div className="form-control">
-          <label className="form-label" htmlFor="about">
+          <label className="form-label" id="about" htmlFor="about">
             About
           </label>
-          <textarea
-            name="about"
-            className="form-input"
-          />
+          <textarea name="about" className="form-input"></textarea>
         </div>
 
         <button className="submit-button" type="submit">
