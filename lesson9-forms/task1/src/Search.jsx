606583bb1ab62b904d30850da4e26381ab3f808f
Volodymyr <babihuk888@gmail.com>
2024-08-22 13:01:53 +0300

    lesson9 task1

---
+++ lesson9-forms/task1/src/Search.jsx
@@ -0,0 +1,25 @@
+  import React, { useState } from 'react';
+
+  const Search = () => {
+    const [searchText, setSearchText] = useState('');
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      alert(searchText)
+    };
+
+    const handleChange = (e) => {
+      setSearchText(e.target.value);
+    }
+
+    return (
+      <form onSubmit={handleSubmit} className="search">
+        <input onChange={handleChange} type="text" className="search__input" value={searchText} />
+        <button className="search__button" type="submit">
+          Search
+        </button>
+      </form>
+    );
+  };
+
+  export default Search;
---
+++ lesson9-forms/task5/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Life from './Life.jsx';
+
+const numbers = [1, 2, 3, 4, 5];
+
+const App = () => {
+  return (
+    <Life />
+  );
+};
+
+export default App;
