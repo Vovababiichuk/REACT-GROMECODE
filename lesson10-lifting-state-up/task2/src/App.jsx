a641dfc0652e070740b4deb0523e703099142abf
Volodymyr <babihuk888@gmail.com>
2024-08-25 01:47:09 +0300

    lesson10 task2

--- lesson10-lifting-state-up/task2/src/App.jsx
+++ lesson10-lifting-state-up/task2/src/App.jsx
@@ -1,35 +1,78 @@
-//! =====================Function Component===================
+// //! =====================Function Component===================
 
-import React, { useState } from 'react';
-import ShoppingCart from './ShoppingCart';
-import Profile from './Profile';
+// import React, { useState } from 'react';
+// import ShoppingCart from './ShoppingCart';
+// import Profile from './Profile';
 
-const App = () => {
-  const [userData, setUserData] = useState({
-    firstName: 'John',
-    lastName: 'Doe',
-  });
+// const App = () => {
+//   const [userData, setUserData] = useState({
+//     firstName: 'John',
+//     lastName: 'Doe',
+//   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setUserData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setUserData((prevData) => ({
+//       ...prevData,
+//       [name]: value,
+//     }));
+//   }
+
+
+//   return (
+//     <div className="page">
+//       <h1 className="title">{`Hello, ${userData.firstName} ${userData.lastName}`}</h1>
+//       <main className="content">
+//         <ShoppingCart userName={userData.firstName} />
+//         <Profile userData={userData} handleChange={handleChange} />
+//       </main>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// //! =====================Class Component===================
+
+import React, { Component } from "react";
+import ShoppingCart from "./ShoppingCart.jsx";
+import Profile from "./Profile.jsx";
+
+class Page extends Component {
+  state = {
+    userData: {
+      firstName: "John",
+      lastName: "Doe",
+    },
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      userData: {
+        ...this.state.userData,
+        [name]: value,
+      },
+    });
+  };
+
+  render() {
+    const { userData } = this.state;
+    return (
+      <div className="page">
+        <h1 className="title">
+          {`Hello, ${userData.firstName} ${userData.lastName}`}
+        </h1>
+        <main className="content">
+          <ShoppingCart userName={userData.firstName} />
+          <Profile
+            userData={userData}
+            handleChange={this.handleChange}
+          />
+        </main>
+      </div>
+    );
   }
+}
 
-
-  return (
-    <div className="page">
-      <h1 className="title">{`Hello, ${userData.firstName} ${userData.lastName}`}</h1>
-      <main className="content">
-        <ShoppingCart userName={userData.firstName} />
-        <Profile userData={userData} handleChange={handleChange} />
-      </main>
-    </div>
-  );
-};
-
-export default App;
-
-//! =====================Class Component===================
+export default Page;
