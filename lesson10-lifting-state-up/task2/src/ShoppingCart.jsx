c2d0fe85c9a9968655d99920af0cdc4e06dc12c1
Volodymyr <babihuk888@gmail.com>
2024-08-25 01:52:23 +0300

    lesson10 task2

--- lesson10-lifting-state-up/task2/src/ShoppingCart.jsx
+++ lesson10-lifting-state-up/task2/src/ShoppingCart.jsx
@@ -4,16 +4,13 @@ import ProductsList from './ProductsList';
 
 const ShoppingCart = ({ userName }) => {
   const [cartItems, setCartItems] = useState([
+    { id: 1, name: 'iPhone 11', price: 999 },
     { id: 2, name: 'iPad Pro', price: 799 },
     { id: 3, name: 'MacBook Pro', price: 1299 },
-    { id: 1, name: 'iPhone 11', price: 999 },
     { id: 4, name: 'Apple Watch', price: 399 },
-    { id: 6, name: 'AirPods', price: 249 },
     { id: 5, name: 'Apple TV', price: 199 },
-    { id: 8, name: 'iMac', price: 9999 },
+    { id: 6, name: 'AirPods', price: 249 },
     { id: 7, name: 'Mac Studio', price: 1999 },
-    { id: 9, name: 'iMac Pro', price: 14999 },
-    { id: 10, name: 'Mac Mini', price: 699 },
   ]);
 
   return (
