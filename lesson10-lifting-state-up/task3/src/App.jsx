4b47cba15919a734a2845a34e7a1ca8453f94b56
Volodymyr <babihuk888@gmail.com>
2024-08-25 02:55:43 +0300

    lesson10 task3

--- lesson10-lifting-state-up/task3/src/App.jsx
+++ lesson10-lifting-state-up/task3/src/App.jsx
@@ -1,71 +1,8 @@
-//! =====================Function Component===================
+import React from 'react';
+import Calculator from './Calculator';
 
-// import React, { useEffect, useState } from 'react';
-// import UserProfile from './UserProfile';
-// import UserMenu from './UserMenu';
+const App = () => {
+  return <Calculator />;
+};
 
-// const App = ({ userId = 'github' }) => {
-//   const [userData, setUserData] = useState(null);
-
-//   useEffect(() => {
-//     fetch(`https://api.github.com/users/${userId}`)
-//       .then(res => {
-//         if (!res.ok) {
-//           throw new Error('Network response was not ok');
-//         }
-//         return res.json();
-//       })
-//       .then(data => setUserData(data))
-//       .catch(err => console.error(err));
-//   }, [userId]);
-
-//   return (
-//     <div className="page">
-//       <header className="header">
-//         <UserMenu userData={userData} />
-//       </header>
-//       <UserProfile userData={userData} />
-//     </div>
-//   );
-// };
-
-// export default App;
-
-//! =====================Class Component===================
-
-import React, { Component } from "react";
-import UserProfile from "./UserProfile.jsx";
-import UserMenu from "./UserMenu.jsx";
-
-class Page extends Component {
-  state = {
-    userData: null,
-  };
-
-  componentDidMount() {
-    this.fetchUserData(this.props.userId);
-  }
-
-  fetchUserData = (userId) => {
-    const userUrl = `https://api.github.com/users/${userId}`;
-    fetch(userUrl)
-      .then((response) => response.json())
-      .then((userData) => {
-        this.setState({
-          userData,
-        });
-      });
-  };
-  render() {
-    return (
-      <div className="page">
-        <header className="header">
-          <UserMenu userData={this.state.userData} />
-        </header>
-        <UserProfile userData={this.state.userData} />
-      </div>
-    );
-  }
-}
-
-export default Page;
+export default App;
