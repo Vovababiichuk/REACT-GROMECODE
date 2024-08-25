341d0771210531ee21987e350e7a67324e94fc93
Volodymyr <babihuk888@gmail.com>
2024-08-25 23:12:24 +0300

    lesson11 task3

--- lesson11-component-updates/task3/src/App.jsx
+++ lesson11-component-updates/task3/src/App.jsx
@@ -1,22 +1,40 @@
-import React, { useState } from 'react';
+//! v1
+
+// import React, { useState } from 'react';
+// import Expand from './Expand';
+
+// const App = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const handleShowContent = () => setIsOpen(prevState => !prevState);
+
+//   return (
+//     <div className="app">
+//       <Expand title="Some title" handleShowContent={handleShowContent} isOpen={isOpen}>
+//         <p>
+//           Hooks are a new addition in React 16.8. They let you use state and other React features
+//           without writing a class.
+//         </p>
+//       </Expand>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+//! v2
+
+import React from 'react';
 import Expand from './Expand';
 
 const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleShowContent = () => {
-    setIsOpen(!isOpen);
-  }
-
   return (
     <div className="app">
-      <Expand title="Some title" handleShowContent={handleShowContent} isOpen={isOpen}>
-        {isOpen && (
-          <p>
-            Hooks are a new addition in React 16.8. They let you use state and other React features
-            without writing a class.
-          </p>
-        )}
+      <Expand title="Some title">
+        <p>
+          Hooks are a new addition in React 16.8. They let you use state and other React features
+          without writing a class.
+        </p>
       </Expand>
     </div>
   );
--- lesson11-component-updates/task3/src/Expand.jsx
+++ lesson11-component-updates/task3/src/Expand.jsx
@@ -1,15 +1,51 @@
-import React from 'react';
+//! v1
 
-const Expand = ({ title, children, handleShowContent, isOpen }) => {
+// import React from 'react';
+
+// const Expand = ({ title, children, handleShowContent, isOpen }) => {
+//   const renderIcon = () => {
+//     return isOpen ? <i className="fas fa-chevron-up"></i> : <i className="fas fa-chevron-down"></i>;
+//   };
+
+//   return (
+//     <div className="expand border">
+//       <div className="expand__header">
+//         <span className="expand__title">{title}</span>
+//         <button onClick={handleShowContent} className="expand__toggle-btn">
+//           {renderIcon()}
+//         </button>
+//       </div>
+//       {isOpen && <div className="expand__content">{children}</div>}
+//     </div>
+//   );
+// };
+
+// export default Expand;
+
+//! v2
+
+import React, { useState } from 'react';
+
+const Expand = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleShowContent = () => {
+    setIsOpen(prevState => !prevState);
+  };
+
+  const renderIcon = () => {
+    return isOpen ? <i className="fas fa-chevron-up"></i> : <i className="fas fa-chevron-down"></i>;
+  };
+
   return (
     <div className="expand border">
       <div className="expand__header">
         <span className="expand__title">{title}</span>
         <button onClick={handleShowContent} className="expand__toggle-btn">
-          {isOpen ? <i className="fas fa-chevron-up"></i> : <i className="fas fa-chevron-down"></i>}
+          {renderIcon()}
         </button>
       </div>
-      <div className="expand__content">{children}</div>
+      {isOpen && <div className="expand__content">{children}</div>}
     </div>
   );
 };
