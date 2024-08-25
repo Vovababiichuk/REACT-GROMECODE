65fbfd6614591649229ae7eb81a92fa8a842a0e1
Volodymyr <babihuk888@gmail.com>
2024-08-25 22:38:05 +0300

    lesson11 task2

--- lesson11-component-updates/task2/src/App.jsx
+++ lesson11-component-updates/task2/src/App.jsx
@@ -1,29 +1,69 @@
-import React, { useState } from 'react';
+//! =================Function component=================
+
+// import React, { useState } from 'react';
+// import Dialog from './Dialog';
+
+// const App = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const handleOpen = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   const handleHide = () => {
+//     setIsOpen(false);
+//   };
+
+//   return (
+//     <div className="app">
+//       <button onClick={handleOpen} className="btn">
+//         Show dialog
+//       </button>
+//       {isOpen && (
+//         <Dialog onClose={handleHide} title="Recommendation">
+//           <p>Use immutable array methods to work with data. It will help to avoid bugs</p>
+//         </Dialog>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+//! =================Class component===================
+
+import React, { Component } from 'react';
 import Dialog from './Dialog';
 
-const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleOpen = () => {
-    setIsOpen(!isOpen);
+class App extends Component {
+  state = {
+    isOpen: false,
   };
 
-  const handleHide = () => {
-    setIsOpen(false);
+  hideDialog = () => {
+    this.setState({
+      isOpen: false,
+    });
   };
 
-  return (
-    <div className="app">
-      <button onClick={handleOpen} className="btn">
-        Show dialog
-      </button>
-      {isOpen && (
-        <Dialog onClose={handleHide} title="Recommendation">
-          <p>Use immutable array methods to work with data. It will help to avoid bugs</p>
+  showDialog = () => {
+    this.setState({
+      isOpen: true,
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <button className="btn" onClick={this.showDialog}>
+          Show dialog
+        </button>
+        <Dialog onClose={this.hideDialog} isOpen={this.state.isOpen} title="Recommendation">
+          <p>Use immutable array methods to work with data. It will help to avoid bugs</p>;
         </Dialog>
-      )}
-    </div>
-  );
-};
+      </div>
+    );
+  }
+}
 
 export default App;
