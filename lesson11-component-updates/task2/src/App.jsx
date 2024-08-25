319e9d4bcbc369100311a60776816b369b4cc29f
Volodymyr <babihuk888@gmail.com>
2024-08-25 22:29:35 +0300

    lesson11 task2

--- lesson11-component-updates/task2/src/App.jsx
+++ lesson11-component-updates/task2/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import Accordion from './Accordion.jsx';
+import React, { useState } from 'react';
+import Dialog from './Dialog';
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleHide = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <Accordion />
+    <div className="app">
+      <button onClick={handleOpen} className="btn">
+        Show dialog
+      </button>
+      {isOpen && (
+        <Dialog onClose={handleHide} title="Recommendation">
+          <p>Use immutable array methods to work with data. It will help to avoid bugs</p>
+        </Dialog>
+      )}
+    </div>
   );
 };
 
