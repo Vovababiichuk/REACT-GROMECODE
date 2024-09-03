39de745aaa35737a0209151eb0d4ea2a2f004c0d
Volodymyr <babihuk888@gmail.com>
2024-09-03 17:01:58 +0300

    lesson14 task1

--- lesson14-react-hooks-overview/task1/src/Counter.jsx
+++ lesson14-react-hooks-overview/task1/src/Counter.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const Counter = () => {
-	const [count, setCount] = useState(17);
+	const [count, setCount] = useState(0);
 
   return (
     <div className="counter">
